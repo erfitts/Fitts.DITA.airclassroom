e8be8a5abb06783d7a8cb9b5120af5770bc0edd6
erfitts <fitts13@vt.edu>
2016-11-20 09:29:13 -0500

    Undergraduate
    Info for WebHelp Responsive

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,13 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-airclassroom.html";
+fl["1"]= "c-conclusion.html";
+fl["2"]= "c-intro.html";
+fl["3"]= "c-undergrad.html";
+fl["4"]= "t-lights.html";
+fl["5"]= "t-mac.html";
+fl["6"]= "t-pay.html";
+fl["7"]= "t-projector.html";
+fl["8"]= "t-sit.html";
+fl["9"]= "ts-schedule.html";
+var doStem = false;searchLoaded = true;
