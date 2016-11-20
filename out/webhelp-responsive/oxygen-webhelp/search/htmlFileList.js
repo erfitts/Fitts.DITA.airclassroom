74fc025650145e5ba96d7adc8ec0907a9e92f8a0
erfitts <fitts13@vt.edu>
2016-11-20 09:33:10 -0500

    Businesses
    Info for WebHelp Responsive

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -1,14 +1,15 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "c-airclassroom.html";
-fl["1"]= "c-conclusion.html";
-fl["2"]= "c-grad.html";
-fl["3"]= "c-intro.html";
-fl["4"]= "c-undergrad.html";
-fl["5"]= "t-lights.html";
-fl["6"]= "t-mac.html";
-fl["7"]= "t-pay.html";
-fl["8"]= "t-projector.html";
-fl["9"]= "t-sit.html";
-fl["10"]= "ts-schedule.html";
+fl["1"]= "c-business.html";
+fl["2"]= "c-conclusion.html";
+fl["3"]= "c-grad.html";
+fl["4"]= "c-intro.html";
+fl["5"]= "c-undergrad.html";
+fl["6"]= "t-lights.html";
+fl["7"]= "t-mac.html";
+fl["8"]= "t-pay.html";
+fl["9"]= "t-projector.html";
+fl["10"]= "t-sit.html";
+fl["11"]= "ts-schedule.html";
 var doStem = false;searchLoaded = true;
